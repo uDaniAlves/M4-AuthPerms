36fcb70132a2500ffdeb5551e9539f38baafc157
uDaniAlves <daniel.augusto.balves@gmail.com>
2021-07-27 23:57:08 +0100

    Finished auth module

---
+++ hooks/useCan.ts
@@ -0,0 +1,23 @@
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+import { validateUserPermissions } from '../utils/validadeUserPermissions'
+
+type UseCanParams = {
+  permissions?: string[]
+  roles?: string[]
+}
+
+export function useCan({ permissions, roles }: UseCanParams) {
+  const { user, isAuthenticated } = useContext(AuthContext)
+
+  if (!isAuthenticated) {
+    return false
+  }
+  const userHasValidPermissions = validateUserPermissions({
+    user,
+    permissions,
+    roles,
+  })
+
+  return userHasValidPermissions
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,12 @@
-import { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
+import { Can } from '../components/Can'
 import { AuthContext } from '../contexts/AuthContext'
 import { setupAPIClient } from '../services/api'
 import { api } from '../services/apiClient'
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext)
+  const { user, signOut } = useContext(AuthContext)
 
   useEffect(() => {
     api
@@ -14,7 +15,15 @@ export default function Dashboard() {
       .catch((err) => console.log(err))
   }, [])
 
-  return <h1>Dashboard: {user?.email}</h1>
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      <button onClick={signOut}>Sign out</button>
+      <Can permissions={['metrics.list']}>
+        <div>Metricas</div>
+      </Can>
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
